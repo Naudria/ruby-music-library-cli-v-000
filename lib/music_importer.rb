db5ebf266a5c20f83f4e25b220052895acef8594
Rima Jean <rima.jean@gmail.com>
2018-03-30 19:11:45 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,10 +1,13 @@
 class MusicImporter
+  #retrieves the path provided to the MusicImporter object
   attr_accessor :path
 
   def initialize(path)
     @path = path
   end
 
+#loads all the MP3 files in the path directory
+#normalizes the filename to just the MP3 filename with no path
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect {|m| m.gsub("#{path}/","")}
   end
