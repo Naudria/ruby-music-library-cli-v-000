42974f1a7a5642a78d984433e6b85324545650e6
Rima Jean <rima.jean@gmail.com>
2018-03-30 19:18:22 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -9,4 +9,10 @@ class MusicImporter
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect {|m| m.gsub("#{path}/","")}
   end
+
+  def import
+  files.each do |file|
+    Song.create_from_filename(file)
+  end
+end
 end
